11c1c06def957575c7a02cc0e1b5b5d37abbbd34
wevekamola <vivek.amola@coloredcow.in>
2017-08-16 15:57:35 +0530

    #4-add-new-guest-form

--- event-and-guest-management.php
+++ event-and-guest-management.php
@@ -6,12 +6,38 @@ Author: ColoredCow
 Author URI: www.coloredcow.com
 Version: 0.1
 */
+add_action('admin_menu', 'menu_pages');
+    add_action( 'admin_enqueue_scripts', 'cc_plugin_scripts' );
+    add_action( 'admin_enqueue_scripts', 'cc_plugin_styles' );
+    
+    function menu_pages(){
+        add_menu_page('Event and Guest Management', 'Event and Guest Management', 'manage_options', 'event_and_guest_management', 'event_and_guest_management');
+        add_submenu_page('event_and_guest_management', 'Add New Guest', 'Add New Guest' , 'manage_options', 'add_new_guest', 'add_new_guest');
+    }
+    
+    function cc_plugin_scripts($hook){
+        if( $hook != 'toplevel_page_event_and_guest_management' ) {
+            return;
+        }
+        wp_enqueue_script( 'cc-bootstrap4-script', plugin_dir_url( __FILE__ ).'/dist/lib/js/bootstrap4.min.js', array( 'jquery'), '1.0.0', true);
+        wp_enqueue_script( 'cc-bootstrap-tether', 'https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js');
+        wp_enqueue_script( 'cc-fontawesome-icons', 'https://use.fontawesome.com/ffc2c94a85.js');
+        wp_localize_script( 'main', 'PARAMS', array( 'ajaxurl' => admin_url('admin-ajax.php') ) );
+    }
+    
+    function cc_plugin_styles($hook){
+        if( $hook != 'toplevel_page_event_and_guest_management' ) {
+            return;
+        }
+        wp_enqueue_style( 'cc-bootstrap4-style', plugin_dir_url( __FILE__ ).'/dist/lib/css/bootstrap4.min.css');
+        wp_enqueue_style( 'cc-fonts','https://fonts.googleapis.com/css?family=Oswald|Marcellus+SC|Roboto|Open+Sans');
+    }
 
-	add_action('admin_menu', 'menu_pages');
-	function menu_pages(){
-	    add_menu_page('Event and Guest Management', 'Event and Guest Management', 'manage_options', 'event_and_guest_management', 'event_and_guest_management' );
-	}
+    
+    function event_and_guest_management(){
+        require_once("add_new_guest.php");
+    }
 
-	function event_and_guest_management(){
-		echo "Hello";		
-	}
+    function add_new_guest(){
+        require_once("add_new_guest.php");
+    }
