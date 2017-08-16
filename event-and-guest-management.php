d77751b07d0473342bb6e656dcd380c7dfd75b9a
himanshu-dhiman <himanshu@coloredcow.in>
2017-08-16 16:44:35 +0530

    resolved requested fixes

--- event-and-guest-management.php
+++ event-and-guest-management.php
@@ -17,34 +17,33 @@ Version: 0.1
 	}
 
 	function create_plugin_database_table() {
-		 global $wpdb;
-		 $events_table_name = $wpdb->prefix . 'events';
-		 $guests_table_name = $wpdb->prefix . 'guests';
-		 
-		 $query_create_events_table = "CREATE TABLE $events_table_name (
-		 event_id mediumint(9)  NOT NULL AUTO_INCREMENT,
-		 event_name varchar(50) NOT NULL,
-		 event_theme varchar(50) NOT NULL,
-		 event_date date NOT NULL,
-		 event_venue varchar(50) NOT NULL,
-		 PRIMARY KEY  (event_id)
-		 );";
-		 
-		 $query_create_guests_table = "CREATE TABLE $guests_table_name (
-		 guest_id mediumint(9)  NOT NULL AUTO_INCREMENT,
-		 guest_name varchar(50) NOT NULL,
-		 guest_email varchar(50) NOT NULL,
-		 guest_phone_number int(10) NOT NULL,
-		 guest_gender varchar(50) NOT NULL,
-		 guest_status varchar(50) NOT NULL,
-		 guest_category varchar(50) NOT NULL,
-		 PRIMARY KEY  (guest_id)
-		 );";
-		 
+		global $wpdb;
+		$events_table_name = $wpdb->prefix . 'events';
+		$guests_table_name = $wpdb->prefix . 'guests';
 
-		 require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-		 dbDelta( $query_create_events_table );
-		 dbDelta( $query_create_guests_table );
+		$query_create_events_table = "CREATE TABLE $events_table_name (
+			event_id mediumint(5)  NOT NULL AUTO_INCREMENT,
+			event_name varchar(50) NOT NULL,
+			event_theme varchar(50) NOT NULL,
+			event_date date NOT NULL,
+			event_venue varchar(250) NOT NULL,
+			PRIMARY KEY  (event_id)
+		);";
+
+		$query_create_guests_table = "CREATE TABLE $guests_table_name (
+			guest_id mediumint(9)  NOT NULL AUTO_INCREMENT,
+			guest_name varchar(50) NOT NULL,
+			guest_email varchar(50) NOT NULL,
+			guest_phone_number int(10) NOT NULL,
+			guest_gender varchar(50) NOT NULL,
+			guest_category varchar(50) NOT NULL DEFAULT 'approved',
+			PRIMARY KEY  (guest_id)
+		);";
+
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $query_create_events_table );
+		dbDelta( $query_create_guests_table );
 	}
 	 
 	register_activation_hook( __FILE__, 'create_plugin_database_table' );
