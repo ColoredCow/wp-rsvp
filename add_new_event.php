1e772a0cfae26124cd763232c8dc30bb7eb54879
wevekamola <vivek.amola@coloredcow.in>
2017-08-18 10:47:44 +0530

    #2 feedback implemented on event form

--- add_new_event.php
+++ add_new_event.php
@@ -6,7 +6,7 @@
 				<form id="add_event_form">
 					<div class="form-group">
 						<div class="col-lg-12">
-							<label for="event-name" class="form-control-label">
+							<label for="event_name" class="form-control-label">
 								Event Name:
 							</label>
 							<input type="text" class="form-control" placeholder="Event Name" name="event_name" id="event_name" maxlength="50" required>
@@ -14,7 +14,7 @@
 					</div>
 					<div class="form-group">
 						<div class="col-lg-12">
-							<label for="event-theme" class="form-control-label">
+							<label for="event_theme" class="form-control-label">
 								Event Theme:
 							</label>
 							<input type="text" class="form-control" placeholder="Theme of Event" name="event_theme" id="event_theme" maxlength="50" required>
@@ -22,7 +22,7 @@
 					</div>
 					<div class="form-group">
 						<div class="col-lg-12">
-							<label for="event-date" class="form-control-label">
+							<label for="event_date" class="form-control-label">
 								Enter Date :
 							</label>
 							<input type="date" class="form-control"  placeholder="Date of Event" name="event_date" id="event_date" required>
@@ -30,7 +30,7 @@
 					</div>  
 					<div class="form-group">
 						<div class="col-lg-12">
-							<label for="event-venue" class="form-control-label">
+							<label for="event_venue" class="form-control-label">
 								Enter Venue :
 							</label>
 							<input type="text" class="form-control" placeholder="Venue of Event" name="event_venue" id="event_venue" maxlength="250" required>
