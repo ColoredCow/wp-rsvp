1f3d16c9c5137be64a4e591ee1662c960d888ed0
Vivek Amola <vivek.amola@coloredcow.in>
2017-09-14 12:57:50 +0530

    #59 edit-guest-and-event-details (#61)

--- show_event.php
+++ show_event.php
@@ -1,15 +1,19 @@
-<div class="heading">All Events</div>
-	<table class="header-table">
-    	<thead>  
-			<tr>
-				<th>S.No</th>  
-			    <th>Name</th>
-			    <th>Theme</th>
-			    <th>Venue</th>
-			    <th>Date</th>
-			    <th>Action</th>
-			</tr> 
-		</thead> 
-	</table>
-<div id="show_all_events"></div>
-	
+<div class="overlay" id="loading"></div>
+<div class="container-fluid">
+	<div id="sucessfull_event_submission"></div> 
+	<div id="edit_event_form"></div>
+	<div class="heading">All Events</div>
+		<table class="header-table">
+	    	<thead>  
+				<tr>
+					<th>S.No</th>  
+				    <th>Name</th>
+				    <th>Theme</th>
+				    <th>Venue</th>
+				    <th>Date</th>
+				    <th>Action</th>
+				</tr> 
+			</thead> 
+		</table>
+	<div id="show_all_events"></div>
+</div>	
---
+++ template/show-event-table.php
@@ -0,0 +1,13 @@
+<table>
+	<thead>  
+		<tr class="tr">  
+			<th><?php echo $Serial_number ?></th>
+			<th><?php echo $event_name ?></th>
+			<th><?php echo $event_theme ?></th>
+			<th><?php echo $event_venue ?></th>
+			<th><?php echo $modify_date ?></th>
+			<th><button type="button" class="btn btn-outline-info btn-sm edit" id="<?php echo $event_id ?>">Update</button>&nbsp;
+			<button type="button" class="btn btn-outline-danger btn-sm send" id="<?php echo $event_id ?>">Delete</button></th>			
+		</tr> 
+	</thead> 
+</table>
